3b05a5858d28b6c748f8e26b8295809a6c77b78e
OS <hoquanghuy0123@gmail.com>
2022-03-05 12:50:48 +0700

    project initialization

---
+++ src/main/java/com/hqh/graduationthesisserver/GraduationThesisServerApplication.java
@@ -0,0 +1,13 @@
+package com.hqh.graduationthesisserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationThesisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationThesisServerApplication.class, args);
+    }
+
+}
