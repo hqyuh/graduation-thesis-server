5ffb5b21b354cd77fd6d210e87790292832df6a1
OS <hoquanghuy0123@gmail.com>
2022-03-07 20:23:49 +0700

    add Constant

---
+++ src/main/java/com/hqh/graduationthesisserver/constant/SecurityConstant.java
@@ -0,0 +1,17 @@
+package com.hqh.graduationthesisserver.constant;
+
+public class SecurityConstant {
+
+    // 5 days
+    public static final long EXPIRATION_TIME = 432_000_000;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified.";
+    public static final String GET_ARRAYS = "HQYUH";
+    public static final String GET_ARRAYS_ADMINISTRATION = "User Management Portal.";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page.";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page.";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+
+}
