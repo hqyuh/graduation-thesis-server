4714c1560ed5ad5ba0a3b665c3a304a10b24b6b5
OS <hoquanghuy0123@gmail.com>
2022-03-08 22:15:30 +0700

    fix JwtAuthorizationFilter

--- src/main/java/com/hqh/graduationthesisserver/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/hqh/graduationthesisserver/filter/JwtAuthorizationFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        if(request.getMethod().equals(OPTIONS_HTTP_METHOD)) {
+        if(request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) {
             response.setStatus(OK.value());
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
