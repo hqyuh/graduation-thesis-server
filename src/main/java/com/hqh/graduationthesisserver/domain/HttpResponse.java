7e3e18a3fcf16f2e069e5832e2cbb2eecf4e3b00
OS <hoquanghuy0123@gmail.com>
2022-03-18 22:07:50 +0700

    [Update]: time http response

--- src/main/java/com/hqh/graduationthesisserver/domain/HttpResponse.java
+++ src/main/java/com/hqh/graduationthesisserver/domain/HttpResponse.java
@@ -1,18 +1,33 @@
 package com.hqh.graduationthesisserver.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class HttpResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    private Date timeStamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
+    public HttpResponse(int httpStatusCode,
+                        HttpStatus httpStatus,
+                        String reason,
+                        String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
