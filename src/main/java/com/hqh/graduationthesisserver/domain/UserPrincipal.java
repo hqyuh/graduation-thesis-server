32544120df81f0f49551cfcdd1a899688ca09872
OS <hoquanghuy0123@gmail.com>
2022-03-31 16:35:47 +0700

    [Update]: get Authority

--- src/main/java/com/hqh/graduationthesisserver/domain/UserPrincipal.java
+++ src/main/java/com/hqh/graduationthesisserver/domain/UserPrincipal.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
@@ -19,9 +20,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return stream(this.user.getAuthorities())
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+        return Collections.singleton(new SimpleGrantedAuthority(this.user.getRoles()));
     }
 
     @Override
