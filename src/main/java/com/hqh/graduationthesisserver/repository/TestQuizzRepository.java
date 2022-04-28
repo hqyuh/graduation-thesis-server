893617fea506aeff52dd6d05c2707142b2da389f
OS <hoquanghuy0123@gmail.com>
2022-04-28 21:52:15 +0700

    [Modify]: change param repository

--- src/main/java/com/hqh/graduationthesisserver/repository/TestQuizzRepository.java
+++ src/main/java/com/hqh/graduationthesisserver/repository/TestQuizzRepository.java
@@ -3,6 +3,7 @@ package com.hqh.graduationthesisserver.repository;
 import com.hqh.graduationthesisserver.domain.TestQuizz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.websocket.server.PathParam;
@@ -13,10 +14,10 @@ import java.util.Optional;
 public interface TestQuizzRepository extends JpaRepository<TestQuizz, Long> {
 
     @Query("SELECT u FROM TestQuizz u WHERE u.activationCode = :code")
-    Optional<TestQuizz> findTestQuizzByActivationCode(@PathParam("code") String code);
+    Optional<TestQuizz> findTestQuizzByActivationCode(@Param("code") String code);
 
     @Query("SELECT u FROM TestQuizz u WHERE u.testName = :name")
-    TestQuizz findTestQuizzByTestName(@PathParam("name") String name);
+    TestQuizz findTestQuizzByTestName(@Param("name") String name);
 
     List<TestQuizz> findTestQuizzByTopicId(Long id);
 
--- src/main/java/com/hqh/graduationthesisserver/repository/UserAnswerRepository.java
+++ src/main/java/com/hqh/graduationthesisserver/repository/UserAnswerRepository.java
@@ -3,6 +3,7 @@ package com.hqh.graduationthesisserver.repository;
 import com.hqh.graduationthesisserver.domain.UserAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.websocket.server.PathParam;
@@ -16,19 +17,19 @@ public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
            "WHERE q.id = u.question.id " +
            "AND q.correctResult = u.isSelected " +
            "AND u.testQuizz.id = :id")
-    float totalNumberOfCorrectAnswersByQuizzId(@PathParam("id") Long id);
+    float totalNumberOfCorrectAnswersByQuizzId(@Param("id") Long id);
 
     @Query("SELECT COUNT(q.milestones) " +
            "FROM Question q, UserAnswer u " +
            "WHERE q.id = u.question.id " +
            "AND u.testQuizz.id = :id")
-    float totalNumberOfAnswersByQuizzId(@PathParam("id") Long id);
+    float totalNumberOfAnswersByQuizzId(@Param("id") Long id);
 
     @Query("SELECT SUM(q.mark) " +
            "FROM Question q, UserAnswer u " +
            "WHERE q.id = u.question.id " +
            "AND q.correctResult = u.isSelected " +
            "AND u.testQuizz.id = :id")
-    float totalMarkByQuizzId(@PathParam("id") Long id);
+    float totalMarkByQuizzId(@Param("id") Long id);
 
 }
